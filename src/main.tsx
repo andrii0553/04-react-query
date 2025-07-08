ca3b328eb6af7c79287e595d0c7f3f28efcc2b18
andrii0553 <baydak0553@gmail.com>
2025-07-08 19:43:43 +0300

    init

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,17 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 /* import "./index.css"; */
-import App from "./components/App/App.tsx";
-import "./components/App/App.module.css";
+import App from "./components/App/App";
 
-createRoot(document.getElementById("root")!).render(
+const queryClient = new QueryClient();
+
+createRoot(document.getElementById("root") as HTMLElement).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   </StrictMode>
 );
